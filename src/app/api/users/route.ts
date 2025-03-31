3147a94488572046a0f77e3a13e99e8b90366f75
abendafi <bendafischulmann@gmail.com>
2025-03-31 15:10:09 +0200

    feat: integrate prisma + users route

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,15 +1,19 @@
 import { NextRequest, NextResponse } from "next/server"
 import { generateFakeUsers } from "@/services/fakerService"
 import { usersSchema } from "@/validation/userSchema"
+import { prisma } from "@/services/db"
 
 export async function GET(req: NextRequest) {
+    const users = generateFakeUsers(5)
 
-  const users = generateFakeUsers(5)
+    const parseResult = usersSchema.safeParse(users)
+    if (!parseResult.success) {
+        return NextResponse.json({ error: parseResult.error.message }, { status: 400 })
+    }
 
-  const parseResult = usersSchema.safeParse(users)
-  if (!parseResult.success) {
-    return NextResponse.json({ error: parseResult.error.message }, { status: 400 })
-  }
+    //await prisma.user.deleteMany()
+    await prisma.user.createMany({ data: parseResult.data })
 
-  return NextResponse.json(parseResult.data, { status: 200 })
+    const allUsersInDB = await prisma.user.findMany()
+    return NextResponse.json(allUsersInDB, { status: 200 })
 }
